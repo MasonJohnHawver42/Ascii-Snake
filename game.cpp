2500f4b61e6a28ba40f11512484344c295a02719
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-04-26 19:12:01 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -2,46 +2,42 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <termios.h>
-#include "enviorment.cpp"
+#include "gameManager.cpp"
 
 class Game
 {
 public:
-  Enviorment* envio;
+  GameManager* gameMnger;
 
   Game(int x, int y, int w, int h)
   {
-    envio = new Enviorment(x, y, w, h);
+    gameMnger = new GameManager(x, y, w, h);
   }
 
   void play()
   {
-      envio->run();
+    initscr();
+    clear();
+
+    noecho();
+    cbreak();
+    curs_set (0);
+    nodelay(stdscr, TRUE);
+
+    gameMnger->run();
+
+    endwin();
   }
 
 };
 
 int main() {
 
-
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
-  //w.ws_col, w.ws_row
+  int width = min((int)(w.ws_col * .8), 60), height = min((int)(w.ws_row * .8), 30);
 
-  Game* test = new Game(2, 1, 60, 30);
-
-  initscr();
-  clear();
-
-  noecho();
-  cbreak();
-  curs_set (0);
-  nodelay(stdscr, TRUE);
-  //keypad(stdscr, true);
-  //getmaxyx(stdscr, test->height, test->width);
-
+  Game* test = new Game((w.ws_col / 2) - (width / 2), (w.ws_row / 2) - (height / 2), width, height);
   test->play();
-  // Clear ncurses data structures
-  endwin();
  }
